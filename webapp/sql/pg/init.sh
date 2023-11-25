d59e003e5929444761377fcc9e18b3b2f709c504
soudai <alftaketomo@gmail.com>
2023-11-25 11:30:22 +0900

    init.sql

--- webapp/sql/pg/init.sh
+++ webapp/sql/pg/init.sh
@@ -13,6 +13,7 @@ ISUCON_DB_USER=${ISUCON13_MYSQL_DIALCONFIG_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON13_MYSQL_DIALCONFIG_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON13_MYSQL_DIALCONFIG_DATABASE:-isupipe}
 
+psql -U isucon -d isupipe -f ./init.sql";
 
 psql -U isucon -d isupipe -c "\COPY isupipe.users FROM ./users.csv DELIMITER ',' CSV";
 psql -U isucon -d isupipe -c "\COPY isupipe.livestreams FROM ./livestreams.csv DELIMITER ',' CSV";
@@ -20,7 +21,7 @@ psql -U isucon -d isupipe -c "\COPY isupipe.tags FROM ./tags.csv DELIMITER ',' C
 psql -U isucon -d isupipe -c "\COPY isupipe.livestream_tags FROM ./livestream_tags.csv DELIMITER ',' CSV";
 psql -U isucon -d isupipe -c "\COPY isupipe.reservation_slots FROM ./reservation_slots.csv DELIMITER ',' CSV";
 psql -U isucon -d isupipe -c "\COPY isupipe.reactions FROM ./reactions.csv DELIMITER ',' CSV";
-psql -U isucon -d isupipe -c "\COPY isupipe.ngwords FROM ./ngwords.csv DELIMITER ',' CSV";
+psql -U isucon -d isupipe -c "\COPY isupipe.ng_words FROM ./ng_words.csv DELIMITER ',' CSV";
 psql -U isucon -d isupipe -c "\COPY isupipe.livecomments FROM ./livecomments.csv DELIMITER ',' CSV";
 
 bash ../pdns/init_zone.sh 
