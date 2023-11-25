7c5a0cfb72e9b889c9e2dde02a1edb75cacc55ce
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2023-11-25 10:50:56 +0900

    update prepare analyze

--- prepare.sh
+++ prepare.sh
@@ -48,13 +48,13 @@ sleep 0.5 && sudo systemctl is-active mysql # serviceの起動失敗確認。即
 
 # ====== go ======
 (
-  cd /home/isucon/webapp/golang
-  make all
+  cd /home/isucon/webapp/go
+  make build
 )
 mkdir -p /home/isucon/log/app
 #sudo logrotate -f /home/isucon/etc/logrotate.d/app
-sudo systemctl restart isucon.go.service
-sleep 0.5 && sudo systemctl is-active isu-go
+sudo systemctl restart isupipe-go.service
+sleep 0.5 && sudo systemctl is-active isupipe-go
 
 now=`date +'%Y-%m-%dT%H:%M:%S'`
 
