ba8d8f2c771b6b92b07446a09c8b96bdb4bb7002
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2023-11-25 13:54:46 +0900

    add export cmd

--- useful_commands/export_images_db_to_file/main.go
+++ useful_commands/export_images_db_to_file/main.go
@@ -13,37 +13,38 @@ import (
 	//_ "github.com/mackee/pgx-replaced"
 )
 
-type Post struct {
-	ID      int    `db:"id"`
-	Mime    string `db:"mime"`
-	ImgData []byte `db:"imgdata"`
+type Icon struct {
+	ID       int64  `db:"id"`
+	UserID   int64  `db:"user_id"`
+	UserName string `db:"user_name"`
+	Image    []byte `db:"image"`
 }
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags | log.Ltime)
 
-	exportDir := flag.String("export-dir", "/tmp/", "export directory")
+	exportDir := flag.String("export-dir", "/home/isucon/webapp/public/img/", "export directory")
 	flag.Parse()
 
-	db, err := GetDBMysql()
+	db, err := GetDBPostgres()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Retrieve image data from the database
-	var posts []Post
-	query := `SELECT id, mime, imgdata FROM posts`
-	err = db.Select(&posts, query)
+	var icons []Icon
+	query := `SELECT icons.id as id, name as user_name , image FROM icons JOIN isupipe.users on icons.user_id = users.id`
+	err = db.Select(&icons, query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Save each image to a file
-	for _, post := range posts {
-		filename := filepath.Join(*exportDir, fmt.Sprintf("%d.%s", post.ID, mimeToExt(post.Mime)))
-		err := os.WriteFile(filename, post.ImgData, 0666)
+	for _, icon := range icons {
+		filename := filepath.Join(*exportDir, fmt.Sprintf("%d.jpeg", icon.UserName))
+		err := os.WriteFile(filename, icon.Image, 0777)
 		if err != nil {
-			log.Printf("Failed to write file for post ID %d: %v\n", post.ID, err)
+			log.Printf("Failed to write file for icon ID %d: %v\n", icon.ID, err)
 			continue
 		}
 		fmt.Printf("File saved: %s\n", filename)
@@ -51,16 +52,3 @@ func main() {
 }
 
 // mimeToExt converts a MIME type to a file extension
-func mimeToExt(mime string) string {
-	switch mime {
-	case "image/jpeg":
-		return "jpg"
-	case "image/png":
-		return "png"
-	case "image/gif":
-		return "gif"
-	// Add more MIME types as needed
-	default:
-		return "bin"
-	}
-}
