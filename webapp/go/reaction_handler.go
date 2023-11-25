37d3c510d9f43163c77f22ef28e7c2a8184a729f
mackee <macopy123@gmail.com>
2023-11-25 16:55:12 +0900

    tx閉じてる

--- webapp/go/reaction_handler.go
+++ webapp/go/reaction_handler.go
@@ -134,11 +134,11 @@ func postReactionHandler(c echo.Context) error {
 	}
 
 	var livestreamModel LivestreamModel
-	if err := tx.GetContext(ctx, &livestreamModel, "SELECT * FROM livestreams WHERE id = ?", livestreamID); err != nil {
+	if err := dbConn.GetContext(ctx, &livestreamModel, "SELECT * FROM livestreams WHERE id = ?", livestreamID); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livestream: "+err.Error())
 	}
 	var userModel UserModel
-	if err := tx.GetContext(ctx, &userModel, "SELECT * FROM users WHERE id = ?", livestreamModel.UserID); err != nil {
+	if err := dbConn.GetContext(ctx, &userModel, "SELECT * FROM users WHERE id = ?", livestreamModel.UserID); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get user: "+err.Error())
 	}
 	sfs.Forget(userModel.Name)
